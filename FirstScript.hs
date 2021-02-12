56f3fe0a64332f8d3b15fd4225637a4e8a3f1b75
rohakapo <36389262+rohakapo@users.noreply.github.com>
2021-02-12 13:23:36 +0100

    Add files via upload

---
+++ FirstScript.hs
@@ -0,0 +1,10 @@
+module FirstScript where
+age int =
+      case int of
+           "Ron" -> "three"
+          -- "Bon" -> "four"
+           --"Son" -> "six"
+           --"Ton" -> "seven"
+           --_ -> "Not my friend"
+
+
